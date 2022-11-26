f13c11d9720290edd6a4667abc32fd028d8146ff
adahmo <118616463+adahmo@users.noreply.github.com>
2022-11-25 22:57:07 -0400

    Update proj2.sh

--- proj2.sh
+++ proj2.sh
@@ -2,3 +2,4 @@ This is the day the Lrd has made and we rejoice and are glad
 I arrrived Port Hawkesbury this evening
 Ann welcome me with a treat at a Chinese restaurant.
 The project seems to be going well
+Yes, kudos to you for the good work amd regards to Ann
