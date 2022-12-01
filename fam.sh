44bf27612127785db9dd1709c8be49977be58551
Adahmo <adamumj@gmail.com>
2022-12-01 21:44:10 +0000

    updated fam

--- fam.sh
+++ fam.sh
@@ -3,4 +3,5 @@
 # I am trusting God for another breakthorugh
 # I am coming to testify
 # we are the family of God
-# we did something of a sacrifice today 
+# we did something of a sacrifice today
+# spent time making sure Jenny is healed of her hurt 
