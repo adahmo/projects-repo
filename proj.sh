bc62add8df15305a24c247687d68934618269857
Adahmo <adamumj@gmail.com>
2022-12-12 18:56:12 +0000

    updated proj2 file

--- proj.sh
+++ proj.sh
@@ -1,3 +1,7 @@
 # Jesus is Lord and King of my life
 # we are the people of the Lord the folk that is laid by his hands
 # come, let us adore him
+# it is the sound of Christmas and all are waiting in expectation
+# time flies as we quickly move closer to christmas
+# what does that potent for us?
+# i think that is the question that should form our minds
