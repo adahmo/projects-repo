c8c9a3dc0cbbaae36d6afff3fc31b879693c192f
Adahmo <adamumj@gmail.com>
2022-12-12 18:20:33 +0000

    update fam

--- fam.sh
+++ fam.sh
@@ -5,3 +5,5 @@
 # we are the family of God
 # we did something of a sacrifice today
 # spent time making sure Jenny is healed of her hurt 
+# this is the day the Lord has made
+# we rejoice and are glad in tndeed!
