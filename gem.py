c91564c6af3839df224e10762dbe1dc61b27ffaa
adahmo <118616463+adahmo@users.noreply.github.com>
2022-12-12 14:37:26 -0400

    Update gem.py

--- gem.py
+++ gem.py
@@ -1,3 +1,5 @@
 # this is a python script
 # i want to list the gem that i have met in my life
 # i just want to test
+# this is exciting to practise
+# we move, and we conque and become experts in our skill.
