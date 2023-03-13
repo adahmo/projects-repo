29a3dd4d29f6a256fd7414fc87bc9f92d957e7e0
adahmo <118616463+adahmo@users.noreply.github.com>
2023-03-13 17:32:36 -0300

    Updated gem.py
    This file has been updated from the source manager.

--- gem.py
+++ gem.py
@@ -4,3 +4,6 @@
 # this is exciting to practise
 # At last the modules have been completed and we await the fruits of our labour
 # We are now in bootcamp hoping to start applying for jobs and getting hired
+# It's been exciting but ovrwhelming for me
+# NOw is the time to revise and submit myself for interviews.
+# Hoping to strict a deal soon! So, help me God!
