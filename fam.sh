11d07ce2806ab2b71fe0e09db15c8242ff0b42cd
Adahmo <adamumj@gmail.com>
2023-03-13 18:37:29 +0000

    modfied files

--- fam.sh
+++ fam.sh
@@ -4,6 +4,8 @@
 # I am coming to testify
 # we are the family of God
 # we did something of a sacrifice today
-# spent time making sure Jenny is healed of her hurt 
-# this is the day the Lord has made
+# I am waiting for the manifestation of the Will of God.
+# For the ways of the Lord is good and gracious
+# The blessing of the Lord add no sorrow or pain. 
+# this is the day the Lord has made#
 # we rejoice and are glad in tndeed!
--- gem.py
+++ gem.py
@@ -2,4 +2,5 @@
 # i want to list the gem that i have met in my life
 # i just want to test
 # this is exciting to practise
-
+# At last the modules have been completed and we await the fruits of our labour
+# We are now in bootcamp hoping to start applying for jobs and getting hired
