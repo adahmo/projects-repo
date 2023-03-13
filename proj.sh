12348f353739b2e179ca22890fa4166b9edf1c8a
Adahmo <adamumj@gmail.com>
2023-03-13 23:32:41 +0000

    updated project file

--- proj.sh
+++ proj.sh
@@ -1,7 +1,6 @@
 # Jesus is Lord and King of my life
 # we are the people of the Lord the folk that is laid by his hands
 # come, let us adore him
-# it is the sound of Christmas and all are waiting in expectation
-# time flies as we quickly move closer to christmas
-# what does that potent for us?
-# i think that is the question that should form our minds
+# Time is not a waiter of anyone and that is true.
+# Pursue your life's goals and make sure you achieve 
+# For God is on your side
